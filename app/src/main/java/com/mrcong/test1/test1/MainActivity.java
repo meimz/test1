e9173dada93a1fc457f6a058c3a9eabb20f9cd06
mrcong <zh-lei@foxmail.com>
2018-09-29 08:21:13 +0800

    测试通知

---
+++ app/src/main/java/com/mrcong/test1/test1/MainActivity.java
@@ -0,0 +1,105 @@
+package com.mrcong.test1.test1;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RemoteViews;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn =(Button) findViewById(R.id.mybtn1);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendNotification();
+                showNotificationProgress(MainActivity.this);
+            }
+        });
+
+    }
+
+
+    private void sendNotification() {
+        //获取NotificationManager实例
+        NotificationManager notifyManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        //实例化NotificationCompat.Builde并设置相关属性
+        Uri soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this,"")
+                //设置小图标
+                .setSmallIcon(R.mipmap.ic_launcher)
+                //设置通知标题
+                .setContentTitle("最简单的Notification")
+                .setSound(soundUri)
+                //设置通知内容
+                .setContentText("只有小图标、标题、内容");
+        //设置通知时间，默认为系统发出通知的时间，通常不用设置
+        //.setWhen(System.currentTimeMillis());
+        //通过builder.build()方法生成Notification对象,并发送通知,id=1
+        notifyManager.notify(1, builder.build());
+    }
+    public static void showNotificationProgress(Context context) {
+        //进度条通知
+        final NotificationCompat.Builder builderProgress = new NotificationCompat.Builder(context,"123");
+        builderProgress.setContentTitle("下载中");
+        builderProgress.setSmallIcon(R.mipmap.ic_launcher);
+        builderProgress.setTicker("进度条通知");
+        builderProgress.setProgress(100, 0, false);
+        final Notification notification = builderProgress.build();
+        Uri uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+
+        notification.sound = uri;
+        notification.ledARGB = Color.RED;// 控制 LED 灯的颜色，一般有红绿蓝三种颜色可选
+        notification.ledOnMS = 1000;// 指定 LED 灯亮起的时长，以毫秒为单位
+        notification.ledOffMS = 1000;// 指定 LED 灯暗去的时长，也是以毫秒为单位
+        notification.flags = Notification.FLAG_SHOW_LIGHTS;// 指定通知的一些行为，其中就包括显示
+
+        final NotificationManager notificationManager = (NotificationManager) context.getSystemService(context.NOTIFICATION_SERVICE);
+        //发送一个通知
+        notificationManager.notify(2, notification);
+        /**创建一个计时器,模拟下载进度**/
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            int progress = 0;
+
+            @Override
+            public void run() {
+                Log.i("progress", progress + "");
+                while (progress <= 100) {
+                    progress++;
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                    //更新进度条
+                    builderProgress.setProgress(100, progress, false);
+                    //再次通知
+                    notificationManager.notify(2, builderProgress.build());
+                }
+                //计时器退出
+                this.cancel();
+                //进度条退出
+                notificationManager.cancel(2);
+                return;//结束方法
+            }
+        }, 0);
+    }
+
+}
